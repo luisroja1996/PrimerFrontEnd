a4751069b14b634dafd7f732b2792e22e65f4e91
D44vs <smcarvajalgu@gmail.com>
2025-08-23 11:29:45 -0500

    Agregar archivos de proyecto.

---
+++ PrimerFrontEnd/Components/Pages/Home.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using PrimerFrontEnd.Models;
+
+namespace PrimerFrontEnd.Components.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        private IHttpClientFactory httpClientFactory { get; set; } = default!;
+        private HttpClient httpClient;
+        private static List<Users> users = new List<Users>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            httpClient = httpClientFactory.CreateClient("UserApi");
+            await GetUsers();
+        }
+
+        private async Task GetUsers()
+        {
+            try
+            {
+                var result = await httpClient.GetAsync("/User/GetUsers");
+
+                if(result.IsSuccessStatusCode)
+                {
+                    users = await result.Content.ReadFromJsonAsync<List<Users>>();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception($"ocurrio un errror: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ PrimerFrontEnd/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimerFrontEnd.Models
+{
+    public class Users
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Categoria { get; set; }
+    }
+}
